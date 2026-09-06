a7418e67cf1d26c8af5ea2cdf5de3a3b169be5c4
isagzbyk46 <101513360+isagzbyk46@users.noreply.github.com>
2026-09-06 14:28:25 +0300

    Update GitHub and Telegram links in dir.py

--- ShrutiMusic/core/dir.py
+++ ShrutiMusic/core/dir.py
@@ -46,8 +46,8 @@ def dirr():
 
 # ===========================================
 # ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
-# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
-# 📢 Telegram Channel : https://t.me/ShrutiBots
+# 🔗 GitHub : https://github.com/isagzbyk46/ShrutiMusic
+# 📢 Telegram Channel : https://t.me/gitaristmusic1
 # ===========================================
 
 
